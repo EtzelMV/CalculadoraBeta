f564bae7ce4885422e94b040b3c023b1853d8eb2
EtzelV <etzelvalderrama@gmail.com>
2023-04-19 10:23:37 -0400

    Funciones suma, resta, multiplicar y dividir. Mostrar resultado en el div.

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
-// const sumar = document.getElementById('sum');
-
-// function funcionQueSuma() {
-//   alert('No tengo ganas');
-// }
-
-
-// sumar.addEventListener('click', funcionQueSuma)
-
-
-
 
 function sumar() {
-  const num1 = document.getElementById('numero1')
-  const num2 = document.getElementById('numero2')
-  console.log(num1, num2);
+  // Obtener los valores de los inputs
+  var num1 = parseFloat(document.getElementById("numero1").value);
+  var num2 = parseFloat(document.getElementById("numero2").value);
+
+  // Calcular la suma
+  var resultado = num1 + num2;
+
+  // Mostrar el resultado en una alerta
+  /*alert("Resultado de sumar " + num1 + " + " + num2 + " = " + resultado);*/
+
+  // Mostrar resultado en el div de resultado
+  const total = document.getElementById('total');
+  total.innerHTML = ` ${num1} + ${num2} = ${resultado}`;
 }
+
+function restar() {
+  // Obtener los valores de los inputs
+  var num1 = parseFloat(document.getElementById("numero1").value);
+  var num2 = parseFloat(document.getElementById("numero2").value);
+
+  // Calcular la resta
+  var resultado = num1 - num2;
+
+  // Mostrar el resultado en una alerta
+  /*alert("Resultado de restar " + num1 + " - " + num2 + " = " + resultado);*/
+
+  // Mostrar resultado en el div de resultado
+  const total = document.getElementById('total');
+  total.innerHTML = ` ${num1} - ${num2} = ${resultado}`;
+}
+
+function multiplicar() {
+  // Obtener los valores de los inputs
+  var num1 = parseFloat(document.getElementById("numero1").value);
+  var num2 = parseFloat(document.getElementById("numero2").value);
+
+  // Calcular la multiplicación
+  var resultado = num1 * num2;
+
+  // Mostrar el resultado en una alerta
+  /*alert("Resultado de multiplicar " + num1 + " * " + num2 + " = " + resultado);*/
+
+  // Mostrar resultado en el div de resultado
+  const total = document.getElementById('total');
+  total.innerHTML = ` ${num1} * ${num2} = ${resultado}`;
+}
+
+function dividir() {
+  // Obtener los valores de los inputs
+  var num1 = parseFloat(document.getElementById("numero1").value);
+  var num2 = parseFloat(document.getElementById("numero2").value);
+
+  // Calcular la división
+  var resultado = num1 / num2;
+
+  // Mostrar el resultado en una alerta
+  /*alert("Resultado de dividir " + num1 + " / " + num2 + " = " + resultado);*/
+
+  // Mostrar resultado en el div de resultado
+  const total = document.getElementById('total');
+  total.innerHTML = ` ${num1} / ${num2} = ${resultado}`;
+}
+
