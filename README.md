8097f9d72f629c294fa60293956cb8344dd45aae
Carlos Pizarro <carlospizarromorales@gmail.com>
2023-04-19 09:22:17 -0400

    Primer commit sprint modulo 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sprint módulo 2: Calculadora
+
+
+
+# Integrantes: 
+
+* Etzel Mencias [Github](https://github.com/EtzelMV)
+* Rosa Nuñez [Github](https://github.com/Rouseandrea)
+* Ismael Vidal [Github](https://github.com/IsmaelVidalBasare)
+* Carlos Pizarro [Github](https://github.com/CarlosPizarroMorales)
---
+++ script.js
@@ -0,0 +1,17 @@
+// const sumar = document.getElementById('sum');
+
+// function funcionQueSuma() {
+//   alert('No tengo ganas');
+// }
+
+
+// sumar.addEventListener('click', funcionQueSuma)
+
+
+
+
+function sumar() {
+  const num1 = document.getElementById('numero1')
+  const num2 = document.getElementById('numero2')
+  console.log(num1, num2);
+}
