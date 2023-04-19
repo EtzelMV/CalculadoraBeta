c87b18efde6ae3e0ae610891929c5ed17211df45
Carlos Pizarro <carlospizarromorales@gmail.com>
2023-04-19 10:30:33 -0400

    cambios menores para borrar

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Sprint módulo 2: Calculadora
-
+Calculadora hermosa que se hace con lo aprendido hasta ahora. 
 
 
 # Integrantes: 
 
-* Etzel Mencias [Github](https://github.com/EtzelMV)
+* Etzel Mencias [Github](https://github.com/EtzelMV/)
 * Rosa Nuñez [Github](https://github.com/Rouseandrea)
 * Ismael Vidal [Github](https://github.com/IsmaelVidalBasare)
 * Carlos Pizarro [Github](https://github.com/CarlosPizarroMorales)
+* María Jesus Oliva [Github](https://github.com/)
--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-// const sumar = document.getElementById('sum');
+/**********************************************
+**                DEFICIONES                 **
+**********************************************/
+const num1 = document.getElementById('numero1');
+const num2 = document.getElementById('numero2');
+const resultado = document.getElementById('resultado');
 
-// function funcionQueSuma() {
-//   alert('No tengo ganas');
-// }
+/**********************************************
+**                 FUNCIONES                 **
+**********************************************/
+function limpiarEntradaUser() {
+  const num1Limpio = num1.value.trim();
+  const num2Limpio = num2.value.trim();
+  
+  // clausula de guarda (guard clause)
+  if (num1Limpio === '' || num2Limpio === '') return;
 
+  const num1Number = Number(num1Limpio)
+  const num2Number = Number(num2Limpio)
 
-// sumar.addEventListener('click', funcionQueSuma)
+  return [num1Number, num2Number];
+}
 
-
+function mostrarResultado(result) {
+  resultado.textContent = result;
+}
 
 
 function sumar() {
-  const num1 = document.getElementById('numero1')
-  const num2 = document.getElementById('numero2')
-  console.log(num1, num2);
+  const numeros = limpiarEntradaUser();
+  mostrarResultado(numeros[0] + numeros[1]);
 }
+function restar() {
+  const numeros = limpiarEntradaUser();
+  mostrarResultado(numeros[0] - numeros[1]);
+}
+function multiplicar() {
+  const numeros = limpiarEntradaUser();
+  mostrarResultado(numeros[0] * numeros[1]);
+}
+function dividir() {
+  const numeros = limpiarEntradaUser();
+  if (numeros[1] === 0) {
+    mostrarResultado('ERROR');
+  } else {
+    mostrarResultado(numeros[0] / numeros[1]);
+  }
+}
+ 
